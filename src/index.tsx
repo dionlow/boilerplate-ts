9f219feb6c8cb0a0818306b31b6f16ebde6d90f0
Dion Low <dionlow17@gmail.com>
2018-08-19 00:02:34 -0700

    Boilerplate Typescript, React, Redux App using create-react-app

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
+import { render } from 'react-dom';
+
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <App />,
+import Hello from './containers/Hello';
+import { Provider } from 'react-redux';
+import { Store, createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+
+const store: Store<StoreState>= createStore(
+  enthusiasm, 
+    { enthusiasmLevel: 1,
+      languageName: 'TypeScript'
+    }
+  );
+
+render(
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
